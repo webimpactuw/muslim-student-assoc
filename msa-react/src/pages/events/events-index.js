0f7be2c383cc293bed9e450f0e1a6ea1bf065328
hithereryan <extmac@gmail.com>
2023-04-20 13:07:49 -0700

    responsive ih-home css

--- msa-react/src/pages/events/events-index.js
+++ msa-react/src/pages/events/events-index.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import EventsHome from "./events-home.js";
 import Activities from "./activities.js";
 import Involved from "./involved.js";
+import Member from "./member.js";
 
 const Events = () => {
     return (
@@ -9,6 +10,7 @@ const Events = () => {
             <Route path="/" element={<EventsHome />} />
             <Route path="activities" element={<Activities />} />
             <Route path="get-involved" element={<Involved />} />
+            <Route path="member" element={<Member />} />
         </Routes>
     );
 };
