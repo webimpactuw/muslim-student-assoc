2e21b8b0d64c7d7e1e5e386db4895e6885d2996b
Daniella <daniellatsing@gmail.com>
2023-05-16 18:31:03 -0700

    added icons and spacing to desktop version of contact us

--- msa-react/src/pages/contact.js
+++ msa-react/src/pages/contact.js
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+import Instagram from "../assets/social-media/1-instagram-purple.svg";
+import Facebook from "../assets/social-media/2-Facebook-purple.svg";
+import Discord from "../assets/social-media/3-discord-purple.svg";
+import Email from "../assets/social-media/4-email-purple.svg";
+import LinkTree from "../assets/social-media/5-linktree-purple.svg";
+
 function Contact() {
     const links = ["", "https://www.instagram.com/msauw/", "https://discord.gg/kP4rYcEPe4", "https://www.facebook.com/uwmsa/", "https:www.linktr.ee/msauw"];
 
@@ -8,30 +15,55 @@ function Contact() {
                 <p className="questions">Feel free to contact us about any questions<br/> 
                 you may have about our organization!</p>
                 <div className="social-media-contact">
-                    <div className="social">
-                        {/* <a href="mailto:msauw@uw.edu">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
-                                <img 
-                                    className="email"
-                                    href="./assets/social-media/4-email.svg" 
-                                    width="100" 
-                                    height="100" 
-                                />
-                            </svg>
-                        </a> */}
-                        <p>msauw@uw.edu</p>
+                    <div className="contact-social">
+                        <a href="mailto:msauw@uw.edu">
+                            <img 
+                                className="contact-social-icon"
+                                alt="Email"
+                                src={Email} 
+                            />
+                        </a>
+                        <p className="contact-social-text">Email: msauw@uw.edu</p>
                     </div>
-                    <div className="social">
-                        <p>@msauw</p>
+                    <div className="contact-social">
+                        <a href="https://www.instagram.com/msauw/">
+                            <img 
+                                className="contact-social-icon"
+                                alt="Instagram"
+                                src={Instagram} 
+                            />
+                        </a>
+                        <p className="contact-social-text">Instagram: @msauw</p>
                     </div>
-                    <div className="social">
-                        <p>msauw</p>
+                    <div className="contact-social">
+                        <a href="https://discord.gg/kP4rYcEPe4">
+                            <img 
+                                className="contact-social-icon"
+                                alt="Discord"
+                                src={Discord} 
+                            />
+                        </a>
+                        <p className="contact-social-text">Discord: msauw</p>
                     </div>
-                    <div className="social">
-                        <p>Muslim Students Association - University of Washington</p>
+                    <div className="contact-social">
+                        <a href="https://www.facebook.com/uwmsa/">
+                            <img 
+                                className="contact-social-icon"
+                                alt="FaceBook"
+                                src={Facebook} 
+                            />
+                        </a>
+                        <p className="contact-social-text">Facebook: uwmsa</p>
                     </div>
-                    <div className="social">
-                        <p>Linktree</p>
+                    <div className="contact-social">
+                        <a href="https://www.linktr.ee/msauw">
+                            <img 
+                                className="contact-social-icon"
+                                alt="LinkTree"
+                                src={LinkTree} 
+                            />
+                        </a>
+                        <p className="contact-social-text">LinkTree: msauw</p>
                     </div>
                 </div>
             </div>
