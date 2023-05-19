ca87f60a1a1b3faee9800c79a03fe91a981f17e8
Daniella <daniellatsing@gmail.com>
2023-05-18 22:13:51 -0700

    completed mobile and desktop for 'where to pray' and desktop for 'halal restaurants'

--- msa-react/src/pages/resources/pray.js
+++ msa-react/src/pages/resources/pray.js
@@ -1,7 +1,56 @@
+import ArrowSvg from "../../assets/arrow-down.svg"; 
+
 function Pray() {
     return (
-        <h1>Where to Pray?</h1>
+        <div>
+            <div className="center-headings">
+                <h1 className="pray-headings">Where to Pray?</h1>
+            </div>
+            <div className="pray-body-section">
+                <div className="about-ih">
+                    <div className="pray-content">
+                        <h3 className="restaurant-pray-name">1. The Islamic House:</h3>
+                        <p>Prayer times are posted to the right.</p>
+                        <h3 className="restaurant-pray-name">2. HUB Sub-Basement Meditation Room</h3>
+                        <p>Take elevators next to the HUB</p>
+                        <p>Starbucks to the sub-basement. Need a Husky Card to access.</p>
+                        <h3 className="restaurant-pray-name">3. Allen Red Research Commons</h3>
+                        <p>Prayer rugs available outside of Allen Red C.</p>
+                        <h3 className="restaurant-pray-name">4. First-Year Programs Office in Mary Gates Hall</h3>
+                    </div>
+                    <div className="pray-content-table" id="wrap">
+                        <h6 className="prayer-times">Prayer Times</h6>
+                        <img 
+                            className="pray-arrow-down"
+                            src={ArrowSvg}
+                            alt="Down Arrow"
+                        />
+                        <iframe 
+                            id="frame"
+                            loading="lazy" 
+                            src="https://masjidal.com/widget/simple/v3?masjid_id=RKxwXOdO" 
+                            type="text/html" 
+                            width="100%" 
+                            height="100%" 
+                            frameborder="0" 
+                            allowtransparency="true"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
 export default Pray;
+
+<div>
+                    <h3>1. The Islamic House:</h3>
+                    <p>Prayer times are posted to the right.</p>
+                    <h3>2. HUB Sub-Basement Meditation Room</h3>
+                    <p>Take elevators next to the HUB</p>
+                    <p>Starbucks to the sub-basement. Need a Husky Card to access.</p>
+                    <h3>3. Allen Red Research Commons</h3>
+                    <p>Prayer rugs available outside of Allen Red C.</p>
+                    <h3>4. First-Year Programs Office in Mary Gates Hall</h3>
+                </div>
--- msa-react/src/pages/resources/restaurants.js
+++ msa-react/src/pages/resources/restaurants.js
@@ -1,6 +1,37 @@
 function Restaurants() {
     return (
-        <h1>Halal Restaurants</h1>
+        <div className="restaurants-overall">
+            <div className="center-headings">
+                <h1 className="halal-restaurants">Halal Restaurants</h1>
+                <h3 className="list-restaurants-heading">List of Halal Restaurants Nearby</h3>
+            </div>
+            <div className="restaurants-body">
+                <div className="halal-restaurants-maps">
+                    <iframe
+                        src="https://www.google.com/maps/d/u/0/embed?mid=1u8ERVmzE7l4eFrDv_rJYdcvPh3Mq_kE&ehbc=2E312F" 
+                        width="480" 
+                        height="600"
+                    />
+                </div>
+                <div className="restaurant">
+                    <h6 className="restaurant-pray-name">1. Aladdin's (Middle Eastern Gyros)</h6>
+                    <p className="restaurant-address-1">4139 University Way NE</p>
+                    <p className="restaurant-address-2">4541 University Way NE</p>
+                    <h6 className="restaurant-pray-name">2. Burger and Kabob Hut (Burgers)</h6>
+                    <p className="restaurant-address-2">4142 University Way NE</p>
+                    <h6 className="restaurant-pray-name">3. Garam Masala (Pakistan/Indian)</h6>
+                    <p className="restaurant-address-2">4537 University Way NE</p>
+                    <h6 className="restaurant-pray-name">4. Shawarma King (Middle Eastern/Gyros)</h6>
+                    <p className="restaurant-address-2">4337 University Way NE</p>
+                    <h6 className="restaurant-pray-name">5. Sultan's (Middle Eastern/Gyros)</h6>
+                    <p className="restaurant-address-2">4222 University Way NE</p>
+                    <h6 className="restaurant-pray-name">6. Persepolis Grill (Persian)</h6>
+                    <p className="restaurant-address-2">5517 University Way NE</p>
+                    <h6 className="restaurant-pray-name">7. Taste of India (Indian)</h6>
+                    <p className="restaurant-address-2">5517 Roosevelt Way</p>
+                </div>
+            </div>
+        </div>
     );
 };
 
