93b31232e8ba85ca73c4a40fcafb404d069cb4d5
hithereryan <extmac@gmail.com>
2023-05-21 17:42:30 -0700

    merge

--- msa-react/src/pages/about/gallery.js
+++ msa-react/src/pages/about/gallery.js
@@ -13,13 +13,18 @@ function Gallery() {
     }, []);
 
     //each gallery entry has a title and array of images, access with post.info and map the images
-    const posts = content ? content.map((post, index) => 
+    const posts = content ? content.map((post, index) =>
         <div key={index}>
             <h2>{post.info}</h2>
 
             <div className="gallery-img-row">
-                {post.pictures.map((pic) => 
-                <img className="gallery-img" src={urlFor(pic).url()}/> )}
+                {post.pictures.map((pic, picIndex) => (
+                <img
+                          key={picIndex}
+                          className="gallery-img-polaroid"
+                          src={urlFor(pic).url()}
+                        />
+                      ))}
             </div>
 
         </div>
